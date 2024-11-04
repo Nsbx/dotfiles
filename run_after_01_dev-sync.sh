512d1f72465ae8f360506477a28c33fcafa7c963
Nicolas Bondoux <8930930+Nsbx@users.noreply.github.com>
2024-11-04 13:39:49 +0100

    Update run_after_01_dev-sync.sh

--- run_after_01_dev-sync.sh
+++ run_after_01_dev-sync.sh
@@ -6,7 +6,7 @@ NC='\033[0m'
 
 if command -v unison >/dev/null 2>&1; then
     echo -e "${GREEN}🔄 Lancement de la synchronisation dev...${NC}"
-    dev-sync
+    unison dev-sync
 else
     echo "⚠️  Unison n'est pas installé. Veuillez d'abord exécuter le script d'installation."
 fi
